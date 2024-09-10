3685bd683ed736a3860deb77f0c5ed19dfcbfcab
Merlijn Vanherck <merlijn.vanherck@bignited.be>
2024-09-10 15:22:47 +0200

    test until opening murder note

--- tests/hackthefuture.spec.ts
+++ tests/hackthefuture.spec.ts
@@ -1,9 +1,46 @@
-import { test, expect } from '@playwright/test';
-import { describe } from 'node:test';
+import { test, expect, Page } from '@playwright/test';
 
-describe('Hack the Future', () => {
-  test('', async ({ page }) => {
-    await page.goto('https://hackthefuture.bignited.be');
-    
-  });
+test('', async ({ page }) => {
+  await page.goto('https://hackthefuture.bignited.be');
+
+  await page.locator('#action-button').click();
+
+  await page.waitForLoadState('load', { timeout: 30000 });
+
+  await page.locator('body').click();
+  
+  await skipTransition(page);
+
+  await page.waitForURL('**/information');
+
+  await page.reload();
+
+  await page.locator('#name').fill('Merlijn');
+
+  await page.locator('#age').fill('20');
+
+  await page.locator('#species').selectOption('reaper');
+
+  await page.locator('#planet').fill('Earth');
+
+  await page.locator('#planet').press('Enter');
+
+  await skipTransition(page);
+
+  await page.waitForURL('**/collect-code');
+  
+  await expect(page.locator('.victim > img')).toBeVisible();
+  await page.locator('.victim > img').click({ position: { x: 325, y: 310 }, force: true, timeout: 60000 });
+
+  
+  await expect(page.locator('.murder')).toBeVisible();
+  const code = await page.locator('.murder').textContent();
+
+  console.log(code);
 });
+
+const skipTransition = async (page: Page) => {
+  await page.waitForURL('**/transition/**');
+  await page.locator('.ski-button').click({ force: true });
+}
+  
