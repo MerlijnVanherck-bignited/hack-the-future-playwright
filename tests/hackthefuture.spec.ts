00a8c660fcb68a5a90eca1ee4137e69a2f37e8a9
Merlijn Vanherck <merlijn.vanherck@bignited.be>
2024-09-10 14:22:45 +0200

    navigate to hackthefuture

--- tests/hackthefuture.spec.ts
+++ tests/hackthefuture.spec.ts
@@ -3,6 +3,7 @@ import { describe } from 'node:test';
 
 describe('Hack the Future', () => {
   test('', async ({ page }) => {
+    await page.goto('https://hackthefuture.bignited.be');
     
   });
 });
